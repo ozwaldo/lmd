08311d432e4f47890702daf482f3881dec671221
ozwaldo <ozwaldo@gmail.com>
2018-09-04 10:50:30 -0500

    Primer Insert

--- index.php
+++ index.php
@@ -1,5 +1,29 @@
 <?php
   require_once "datos/conexion.php";
 
-  print_r(Conexion::getInstancia());
+  //print_r(Conexion::getInstancia());
+
+$pdo = Conexion::getInstancia()->conectar();
+
+$sql = "INSERT INTO usuario (nombre,contrasena,claveApi,correo)".
+      " VALUES (?,?,?,?)";
+$query = $pdo->prepare($sql);
+$nombre = "juan";
+$query->bindParam(1,$nombre);
+$password = password_hash("123",PASSWORD_BCRYPT);
+echo $password;
+$query->bindParam(2,$password);
+$clave = MD5("juan123");
+$query->bindParam(3,$clave);
+$email = "juan@mail.com";
+$query->bindParam(4,$email);
+$result = $query->execute();
+if ($result) {
+  echo "Operación exitosa.";
+} else {
+  echo "Error al realizar la operación.";
+}
+
+
+
 ?>
